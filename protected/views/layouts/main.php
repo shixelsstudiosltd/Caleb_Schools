35928d02129237c30be3fbbbc8b11e159764b343
Imran Ahmed <hunzai@centangle.com>
2014-08-19 19:53:23 +0500

    Front End Changes Suggested by the Customer

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,59 +1,112 @@
-<?php /* @var $this Controller */ ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Caleb Portal</title>
+<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl;?>/css/style.default.css" type="text/css" />
 
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
-
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-        <?php Yii::app()->bootstrap->register(); ?>
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl;?>/css/responsive-tables.css">
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl;?>/js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl;?>/js/jquery-ui-1.9.2.min.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl;?>/js/bootstrap.min.js"></script>
+<!--[if lte IE 8]><script language="javascript" type="text/javascript" src="<?php echo Yii::app()->request->baseUrl;?>/js/excanvas.min.js"></script><![endif]-->
+<link rel="shortcut icon" type="image/png" href="<?php echo Yii::app()->request->baseUrl;?>/images/favicon.ico"/>
 </head>
 
 <body>
 
-<div class="container" id="page">
+<!-- Bootstrap core CSS  Bootstrap is registered by Yiistrap-->
+<?php  Yii::app()->bootstrap->register(); ?>
+<!--[if lt IE 7]>
+<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
+    your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to
+    improve your experience.</p>
+<![endif]-->
+<div class="mainwrapper">
+    <div class="header">
+            <div class="logo header-top-left">
+                    <img src="images/header-top-left.jpg" alt="" />
+            </div>
+        <div class="headerinner">
+            <ul class="headmenu">
+                <li class="odd">
+                    <div class="logo">
+                        <a href="#"><img src="images/logo.jpg" alt="" /></a>
+                    </div>
+                </li>
+                <li class="right">
+                    <div class="userloggedinfo">
+                        <img src="images/photos/header-temp-image.png" alt="user-image" />
+                        <div class="userinfo">
+                            <p>Jola Ogunsola</p>
+                        </div>
+                            <a href="#"><div class="header-menu-icon"></div></a>
+                    </div>
+                </li>
+            </ul><!--headmenu-->
+        </div>
+    </div>
+    
+    <div class="leftpanel">
+        
+        <div class="leftmenu">        
+            <ul class="nav nav-tabs nav-stacked">
+                <li class="active"><a href="dashboard.html"><span class="iconfa-home"></span> Dashboard</a></li>
+                <li><a href="#"><span class="iconfa-book"></span> Courses</a></li>
+                <li><a href="#"><span class="iconfa-group"></span> Teachers</a></li>
+                <li><a href="#"><span class="iconfa-envelope"></span> Messages</a></li>
+                <li><a href="#"><span class="iconfa-cog"></span> Settings</a></li>
+                <li><a href="#"><span class="iconfa-bar-chart"></span> Assessments</a></li>
+                <li><a href="#"><span class="iconfa-question-sign"></span> FAQS</a></li>
+                <li class="empty-nav"><a href="#"></a></li>
+                <li><a href="#"><span class="iconfa-signout"></span> Sign Out</a></li>
+            </ul>
+        </div><!--leftmenu-->
+        
+    </div><!-- leftpanel -->
+    
+    <div class="rightpanel">
+        <?php echo $content; ?>
+    </div><!--rightpanel-->
+    
+</div><!--mainwrapper-->
 
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
 
-	<?php echo $content; ?>
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<!-- <script>
+     var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+     (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+     g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+     s.parentNode.insertBefore(g,s)}(document,'script'));
+ </script>-->
 
-	<div class="clear"></div>
-
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
-
-</div><!-- page -->
-
+<script type="text/javascript">
+    jQuery(document).ready(function() {
+        
+      // simple chart
+		var flash = [[0, 11], [1, 9], [2,12], [3, 8], [4, 7], [5, 3], [6, 1]];
+		var html5 = [[0, 5], [1, 4], [2,4], [3, 1], [4, 9], [5, 10], [6, 13]];
+      var css3 = [[0, 6], [1, 1], [2,9], [3, 12], [4, 10], [5, 12], [6, 11]];
+			
+		function showTooltip(x, y, contents) {
+			jQuery('<div id="tooltip" class="tooltipflot">' + contents + '</div>').css( {
+				position: 'absolute',
+				display: 'none',
+				top: y + 5,
+				left: x + 5
+			}).appendTo("body").fadeIn(200);
+		}
+        
+        //datepicker
+        jQuery('#datepicker').datepicker();
+        
+        // tabbed widget
+        jQuery('.tabbedwidget').tabs();
+    });
+</script>
 </body>
 </html>
+
+
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,20 +1,35 @@
-<?php
-/* @var $this SiteController */
-
-$this->pageTitle=Yii::app()->name;
-?>
-
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<div class="span4 home-col-space">
+        <div class="headtitle">
+                <div class="btn-group">
+                        <button class="btn dropdown-toggle"><i class="iconfa-cog grey-icon"></i></button>
+                </div>
+                <h4 class="widgettitle">Announcements</h4>
+        </div>
+</div>
+<div class="span4 home-col-space">
+        <div class="headtitle">
+                <div class="btn-group">
+                        <button class="btn dropdown-toggle"><i class="iconfa-cog grey-icon"></i></button>
+                </div>
+                <h4 class="widgettitle">Classes</h4>
+        </div>
+</div>
+<div class="span4 home-col-space">
+        <div class="headtitle">
+                <div class="btn-group">
+                        <button class="btn dropdown-toggle"><i class="iconfa-cog grey-icon"></i></button>
+                </div>
+                <h4 class="widgettitle">Calender</h4>
+                <div class="widgetcontent nopadding">
+                        <div id="datepicker"></div>
+                </div>
+        </div>
+</div>
+<div class="span4 home-col-space">
+        <div class="headtitle">
+                <div class="btn-group">
+                        <button class="btn dropdown-toggle"><i class="iconfa-cog grey-icon"></i></button>
+                </div>
+                <h4 class="widgettitle">Messages</h4>
+        </div>
+</div>
