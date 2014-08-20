f035d9f0c18ce306910da65ced3b21802e7a70bc
Mushahid Hussain <mushahidh@yahoo.com>
2014-08-20 16:00:35 +0500

    .htaccess changed to remove index.php from url

--- protected/config/main.php
+++ protected/config/main.php
@@ -125,16 +125,16 @@ return array(
             ),	
                 
             // uncomment the following to enable URLs in path-format	
-            'urlManager'=>array(
-                    'urlFormat'=>'path',
-                    //'showScriptName' => false,
-                    'rules'=>array(
-                            // 'site/page/<view:\w+>' => 'site/page/',
-                            '<controller:\w+>/<id:\d+>'=>'<controller>/view',
-                            '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-                            '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-                    ),
+           'urlManager' => array(
+            'urlFormat' => 'path',
+            'showScriptName' => false,
+            'rules' => array(
+                'site/page/<view:\w+>' => 'site/page/',
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
             ),
+        ),
             
             /*
             'db'=>array(
